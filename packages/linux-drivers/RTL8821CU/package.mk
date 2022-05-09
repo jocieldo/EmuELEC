58a788c02e59ce276e11200fe2e539aa2299a38e
Jocieldo Abreu <jabreu@soap.health>
2022-05-09 08:35:07 -0300

    fixed rtl8821cu driver.

---
+++ packages/linux-drivers/RTL8821CU/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8821CU"
+PKG_VERSION="37e27f9165300c89607144b646545fac576ec510"
+PKG_SHA256="749ba2e77d0364381445e40f7f0b7041d861cf9a356dbe53085e3abaef888c1d"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/brektrou/rtl8821CU"
+PKG_URL="https://codeload.github.com/brektrou/rtl8821CU/zip/refs/heads/master"
+PKG_LONGDESC="Realtek RTL8821CU Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=${TARGET_KERNEL_ARCH} \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=${TARGET_KERNEL_PREFIX} \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+    cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
+}
