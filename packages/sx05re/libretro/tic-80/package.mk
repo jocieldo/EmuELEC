8bba9b5499e2f0978b80b16f42476c9014aeb83b
Jocieldo Abreu <jabreu@soap.health>
2022-05-11 14:05:17 -0300

    fixed tic-80 installation package

--- packages/sx05re/libretro/tic-80/package.mk
+++ packages/sx05re/libretro/tic-80/package.mk
@@ -1,24 +1,25 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
-
 PKG_NAME="tic-80"
-PKG_VERSION="3a4c813d49f110d701d98fd0d96decb5ca0cbf05"
-PKG_LICENSE="MIT"
-PKG_SITE="https://github.com/nesbox/TIC-80"
-PKG_URL="$PKG_SITE.git"
+PKG_VERSION="e9f62f85a154796c6baaee8a9f6fd0cfdd447019"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/TIC-80"
+PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="TIC-80 is a fantasy computer for making, playing and sharing tiny games."
-GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="cmake"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_LIBRETRO=ON \
-					   -DBUILD_PLAYER=ON \
-					   -DBUILD_DEMO_CARTS=OFF \
+PKG_CMAKE_OPTS_TARGET="-DBUILD_PLAYER=OFF \
+                       -DBUILD_SDL=OFF \
                        -DBUILD_SOKOL=OFF \
-                       -DBUILD_SDL=ON \
+                       -DBUILD_DEMO_CARTS=OFF \
+                       -DBUILD_LIBRETRO=ON \
                        -DBUILD_WITH_MRUBY=OFF \
                        -DCMAKE_BUILD_TYPE=Release"
 
+pre_configure_target() {
+  PKG_CMAKE_SCRIPT="${PKG_BUILD}/core/CMakeLists.txt"
+}
+
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/.$TARGET_NAME/lib/tic80_libretro.so $INSTALL/usr/lib/libretro/tic80_libretro.so
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v lib/tic80_libretro.so ${INSTALL}/usr/lib/libretro/
 }
